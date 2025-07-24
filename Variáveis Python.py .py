f2e2bcc5544d5d07489ec71474503b055a3403c3
jops-hub <jops.jz@gmail.com>
2025-07-24 20:50:32 -0300

    Add files via upload
    Aprendendo a colocar variáveis , sumando as variáveis e resolvendo problemas usando a raiz quadrada no Python. Fico feliz, já que estou aprendendo coisas novas em Python.

---
+++ Variáveis Python.py .py
@@ -0,0 +1,17 @@
+var = 1
+saldo_da_conta = 1000,0
+nome_do_cliente = 'John Doe'
+print(var , saldo_da_conta , nome_do_cliente)
+print(var)
+print()
+var = "3.8.5"
+print("Vesao Python: " + var)
+print()
+var = 100
+var = 200 + 300
+print(var)
+print()
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
